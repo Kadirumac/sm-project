5546cf2a6e5c5a89ad30c584f8fffa10fae0e997
kadirumac <kadirumac49@gmail.com>
2020-09-23 10:08:21 +0200

    computer change

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import DesktopNavbar from './components/Navbar/Navbar'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <DesktopNavbar />
+
+
+
+    </div>
+  );
+}
+
+export default App;
